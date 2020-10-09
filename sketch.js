296a5521b066e152e5d80373a54534e08aec425a
Padmak51 <pkhasage@gmail.com>
2020-10-09 20:05:20 +0530

    comments

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var bg,bgI;
-
+//to load images
 function preload(){
     bgI = loadImage("sprites/bg.png");
 }
@@ -16,7 +16,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    //create ground
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
